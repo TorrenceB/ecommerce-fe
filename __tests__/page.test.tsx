cdd151713fc180d720fc14ea59318edb2b9a94cd
TorrenceB <torrenceb90@gmail.com>
2025-01-29 16:18:12 -0700

    Add sample test for page component

---
+++ __tests__/page.test.tsx
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import Home from "@/app/page";
+
+describe("Home component", () => {
+	test("renders correctly", () => {
+		render(<Home />);
+		expect(false).toBe(false);
+	});
+});
