ad3ce16219e074e2ea2a6465dfd42aaf76f72608
TorrenceB <torrenceb90@gmail.com>
2025-02-23 23:02:59 +0000

    update home content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-	return <div>Store Home Page</div>;
+	return (
+		<div className="flex flex-col items-center justify-center h-screen">
+			<h1 className="text-4xl">Store Coming Soon!</h1>
+		</div>
+	);
 }
